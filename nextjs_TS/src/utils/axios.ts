41b7fd3a0b4e1625b062522bea31825bcba87497
Steven Zeiler <me@stevezeiler.com>
2022-03-02 08:17:07 +0100

    Registration, Login, Password Reset [TODO] Handle Password Reset Link

---
+++ nextjs_TS/src/utils/axios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// ----------------------------------------------------------------------
+
+//const axiosInstance = axios.create({ baseURL: process.env.HOST_API_KEY || '' });
+const axiosInstance = axios.create({ baseURL: 'http://localhost:8000/v1/api' || '' });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+);
+
+export default axiosInstance;
