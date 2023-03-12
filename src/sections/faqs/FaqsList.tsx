bc27557804f042216fcfbd7c535ecad28970f923
Eddie Willers <derrick@pow.co>
2023-03-12 16:48:59 +0100

    Answered the FAQs

--- src/sections/faqs/FaqsList.tsx
+++ src/sections/faqs/FaqsList.tsx
@@ -14,28 +14,28 @@ interface FAQ {
 
 const faqs1: FAQ[] = [{
   question: 'What is the value of Anypay?',
-  answer: '',
+  answer: 'Anypay simplifies your life and reduces headaches by enabling your business to serve crypto customers with a secure, easy-to-use app.',
 }, {
   question: 'What does Anypay do?',
-  answer: ''
+  answer: 'Anypay takes your various different crypto payment methods and converts them into a simple QR code that customers scan to pay.'
 }, {
   question:  'What services does Anypay offer?',
-  answer: ''  
+  answer: 'Anypay offers a merchant app for instantly processing in-person crypto transactions with perfect clarity. Anypay also offers an API for app developers to integrate simple, multi-coin payments into any application without the complexity of managing all that yourself.'  
 }, {
   question:   'Why should I use Anypay?',
-  answer: ''  
+  answer: 'You should use Anypay if you want a professional way to accept crypto payments at your business. It is clear and easy for staff to use without any crypto knowledge. It tracks all your business transactions for accurate accounting. It delights your customers by giving them a fast and easy way to pay.'  
 }, {
   question:   'Who is Anypay for?',
-  answer: ''
+  answer: 'Anypay is for merchants who want instant, reliable crypto payments. Especially if you want to offer more than one coin for a payment option. It works for small businesses who operate a farmers markets as well as big businesses processing thousands of transactions.'
 }, {
   question:   'How much does it cost?',
-  answer: ''  
+  answer: 'Anypay\s basic software is free to download. Processing transactions through the merchant app costs $0.01 per transaction. Using the API costs 0.1% per transaction.'  
 }, {
   question:   'How does it work?',
-  answer: ''  
+  answer: 'Anypay faciliates communication between the customer\'s crypto wallet and the merchant\'s crypto wallet. It helps them talk to each other super-fast about what coins are accepted, what fee to use, what addresses to pay, in what quantities, and when payment is successful. All in the blink of an eye.'  
 }, {
   question:   'Is it custodial or not?',
-  answer: ''
+  answer: 'Anypay is 100% non-custodial. That means Anypay never takes "custody" of your coins. All payments go directly from customer to merchant, without going through a third party. These transactions are also called "Peer-to-peer" and are the most secure.'
 }, {
   question:   'Do I have to KYC?',
   answer: ''
@@ -43,28 +43,28 @@ const faqs1: FAQ[] = [{
 
 const faqs2: FAQ[] = [{
   question: 'Are there limits?',
-  answer: '',
+  answer: 'All transactions over $0.04 are valid on all chains except BTC ($1.00 min). The upper limit is the total amount of currency in circulation.',
 }, {
-  question: 'Can businesses consider as high-risk use it (e.g.: cannabis dispensary)?',
-  answer: ''
+  question: 'Can businesses considered high-risk use it (e.g.: cannabis dispensary)?',
+  answer: 'Yes. Businesses that banks consider "high-risk" such as cannabis dispensaries can use Anypay to accept crypto payments.'
 }, {
   question:  'What cryptocurrencies can I use?',
-  answer: ''  
+  answer: 'Anypay supports Bitcoin Core (BTC), Litecoin (LTC), Dogecoin (DOGE), Bitcoin Cash (BCH), Monero (XMR), Dash (DASH), and Bitcoin SV (BSV)'  
 }, {
   question:   'Can I add Lightning and Liquid?',
-  answer: ''  
+  answer: 'Contact us directly if you need Lightning or Liquid integrations for your business.'  
 }, {
   question:   'Where can I find documentation and tutorials?',
-  answer: ''
+  answer: 'Developers can find API documentation at api.anypayx.com. Merchant app users can find tutorials on YouTube.'
 }, {
   question:   'Do you have integrations with e-commerce platforms? Which ones?',
-  answer: ''  
+  answer: 'Anypay has WordPress plugin that works with WooCommerce. However, this plugin is undergoing an update and is unavailable to new users.'  
 }, {
-  question:   'How do I get support and how do I get in touch with your company?',
-  answer: ''  
+  question:   'How do I get support, and how do I get in touch with your company?',
+  answer: 'Email Anypay Support at support@anypayx.com for direct answers to your questions. Message in the public Anypay Telegram group at t.me/anypayx for the fastest response.'  
 }, {
-  question:   'If you encounter an issue with your software, do your end the development of your platform, or do you work on it and improve it?',
-  answer: ''
+  question:   'If you encounter an issue with your software, do you work on it and improve it?',
+  answer: 'Anypay rarely encounters problems and is long-term stable. When users encounter an issue, we quickly resolve it with an update.'
 }]
 
 interface AccordianItem {
