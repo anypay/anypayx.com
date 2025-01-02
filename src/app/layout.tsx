c1b0f59f8a2622b13be6224bdb822c15981be006
My CTO <mycto@proton.me>
2025-01-02 14:28:17 +0100

    Install google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata, Viewport } from 'next'
 import { Analytics } from "@vercel/analytics/react"
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export const metadata: Metadata = {
   title: {
     template: '%s | Anypay',
-    default: 'Anypay - Bitcoin Payments',
+    default: 'Anypay - Web3 Payments',
   },
   description: 'Bitcoin, Ethereum, Stablecoins Payments Gateway for Apps & Websites',
   metadataBase: new URL('https://www.anypayx.com'),
@@ -26,6 +28,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body>
         {children}
         <Analytics />
+        <GoogleAnalytics gaId="G-1E4TY6R8D4" />
       </body>
     </html>
   )
